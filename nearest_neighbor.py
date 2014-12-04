02a95f7b02770e7659a5ca99eae3602cb8d22294
Jeremy Neal <jneal1992@gmail.com>
2014-12-04 18:45:38 -0500

    Imported static graph into nearest neighbor.

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -4,7 +4,7 @@ Michael Neary & Jeremy Neal
 Traveling Salesman Problem
 '''
 import time
-from graph_generator import GraphGenerator
+# from graph_generator import GraphGenerator
 import random
 
 '''
@@ -13,18 +13,29 @@ inner dictionary are vertices the key is connected to and associated weights
 no self-edges allowed
 '''
 
-graph = {
-    'A': {'B': 86, 'C': 178, 'D': 195},
-    'B': {'A': 86, 'C': 123, 'D': 107, 'E': 171},
-    'C': {'A': 178, 'B': 123, 'E': 170},
-    'D': {'A': 195, 'B': 107, 'E': 210, 'F': 210, 'G': 135, 'H': 64},
-    'E': {'B': 171, 'C': 170, 'D': 210, 'G': 230, 'F': 230},
-    'F': {'D': 210, 'E': 230, 'G': 85},
-    'G': {'D': 135, 'E': 230, 'F': 85, 'H': 67},
-    'H': {'D': 64, 'G': 67}
-}
+# graph = {
+#     'A': {'B': 86, 'C': 178, 'D': 195},
+#     'B': {'A': 86, 'C': 123, 'D': 107, 'E': 171},
+#     'C': {'A': 178, 'B': 123, 'E': 170},
+#     'D': {'A': 195, 'B': 107, 'E': 210, 'F': 210, 'G': 135, 'H': 64},
+#     'E': {'B': 171, 'C': 170, 'D': 210, 'G': 230, 'F': 230},
+#     'F': {'D': 210, 'E': 230, 'G': 85},
+#     'G': {'D': 135, 'E': 230, 'F': 85, 'H': 67},
+#     'H': {'D': 64, 'G': 67}
+# }
 
-graph = GraphGenerator(10).generateGraph()
+graph = {
+    u'East Ricki': {u'Kossbury': 55, u'West Laverneland': 26},
+    u'South Jaylene': {u'Lonnaborough': 93, u'Lake Brody': 96, u'South Katherin': 44},
+    u'West Deenahaven': {u'North Kerrie': 60, u'West Laverneland': 95, u'Rociomouth': 47},
+    u'Kossbury': {u'West Laverneland': 70, u'North Kerrie': 40, u'South Katherin': 43, u'East Ricki': 55, u'Rociomouth': 76},
+    u'North Kerrie': {u'Kossbury': 40, u'West Deenahaven': 60, u'South Katherin': 57},
+    u'South Katherin': {u'Kossbury': 43, u'North Kerrie': 57, u'South Jaylene': 44, u'Lake Brody': 61, u'West Laverneland': 26},
+    u'Lonnaborough': {u'Lake Brody': 74, u'South Jaylene': 93, u'West Laverneland': 96, u'Rociomouth': 74},
+    u'West Laverneland': {u'West Deenahaven': 95, u'Kossbury': 70, u'East Ricki': 26, u'Lonnaborough': 96, u'Lake Brody': 76, u'South Katherin': 26},
+    u'Lake Brody': {u'Lonnaborough': 74, u'West Laverneland': 76, u'South Jaylene': 96, u'South Katherin': 61},
+    u'Rociomouth': {u'Kossbury': 76, u'Lonnaborough': 74, u'West Deenahaven': 47}
+}
 
 # recursive brute force solution to the TSP
 
@@ -67,10 +78,11 @@ def find_all_paths(graph, currentNode, distanceSoFar, pathSoFar):
                 # of the path
                 find_all_paths(graph, node, distanceSoFar, list(pathSoFar))
 
+# graph = GraphGenerator(10).generateGraph()
 startTime = time.time()
 bruteForcePaths = []
 startNode = random.choice(list(graph.iterkeys()))
-print "Starting from node", startNode
+print "Starting from", startNode
 find_all_paths(graph, startNode, 0, [])
 # find_all_paths(graph, 'B', 0, [])
 endTime = time.time()
