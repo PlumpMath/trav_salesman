fc08003d4471902e39042bc3f12f40893966fa80
Jeremy Neal <jneal1992@gmail.com>
2014-12-04 18:07:40 -0500

    Completed graph generator.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Travelling Salesman Solutions
 
 Shenanigans in AI with Mike! :D
+
+This is using Python 2.7 (probably shouldn't.)
+
+Install dependencies with `pip install`.
---
+++ graph_generator.py
@@ -0,0 +1,55 @@
+import math
+import random
+from faker import Factory
+
+
+class GraphGenerator:
+    '''
+    This class generates a graph representing cities for the travelling
+    salesman problem. Given a number of cities, this class can generate a
+    graph with that number of cities and a number of randomly assigned
+    edges.
+    '''
+
+    def __init__(self, num):
+        self.num = num
+        self.graph = {}
+
+    def __str__(self):
+        output = ""
+        for k, v in self.graph.iteritems():
+            output += str(k) + ": " + str(v) + "\n"
+        return output
+
+    def generateGraph(self):
+        '''
+        Generates a graph with random city names and distances.
+        The number of edges is the number of cities raised to to 3/2.
+        Note: this generates an undirected graph.
+        '''
+        faker = Factory.create()
+
+        for i in range(self.num):
+            # Generate random city name.
+            randomCityName = faker.city()
+            self.graph[randomCityName] = {}
+
+        # Pick a random city, add an edge of random length to another city.
+        for i in range(int(math.ceil(self.num ** 1.5))):
+            source = random.choice(list(self.graph.iterkeys()))
+            dest = random.choice(list(self.graph.iterkeys()))
+            if source == dest:
+                pass
+            else:
+                distance = random.randrange(10, 100)
+                self.graph[source][dest] = distance
+                self.graph[dest][source] = distance
+
+
+def main():
+    graph = GraphGenerator(10)
+    graph.generateGraph()
+    print graph
+
+if __name__ == "__main__":
+    main()
